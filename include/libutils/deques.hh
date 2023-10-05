e60a7aefed2727d3ec90a24ddb0461626ae0c8c4
Refolli Francesco <f.refolli@campus.unimib.it>
2023-10-05 16:28:20 +0200

    deques

--- include/libutils/deques.hh
+++ include/libutils/deques.hh
@@ -1,12 +1,12 @@
-#ifndef LIBUTILS_VECTORS_HH
-#define LIBUTILS_VECTORS_HH
+#ifndef LIBUTILS_DEQUES_HH
+#define LIBUTILS_DEQUES_HH
 
-#include <vector>
+#include <deque>
 #include <ostream>
 #include <algorithm>
 
 template<typename T>
-std::ostream& operator<<(std::ostream& out, std::vector<T>& objects) {
+std::ostream& operator<<(std::ostream& out, std::deque<T>& objects) {
     for (auto it = objects.begin(); it != objects.end(); ++it) {
         if (it != objects.begin())
             out << " ";
@@ -16,7 +16,7 @@ std::ostream& operator<<(std::ostream& out, std::vector<T>& objects) {
 }
 
 template<typename T>
-unsigned int vector_index(std::vector<T>& objects, T object) {
+unsigned int deque_index(std::deque<T>& objects, T object) {
     auto it = std::find(objects.begin(), objects.end(), object);
     return std::distance(objects.begin(), it);
 }
--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,7 @@
 #include <libutils/strings.hh>
 #include <libutils/vectors.hh>
 #include <libutils/sets.hh>
+#include <libutils/deques.hh>
 #include <iostream>
 
 std::string to_string(bool value) {
@@ -19,6 +20,7 @@ int main(int argc, char**args) {
     std::cout << "someName  = " << encodeFlagLongName("someName") << std::endl;
     std::string magic_string = "a,b,c";
     std::vector<std::string> magic_vector = tokenizeString(magic_string, ",");
+    std::deque<int> magic_deque = {1, 2, 3, 'c'};
     std::cout << magic_vector << std::endl;
     std::set<int> A = {0, 1, 2};
     std::set<int> B = {2};
@@ -28,4 +30,6 @@ int main(int argc, char**args) {
     std::cout << C << std::endl;
     set_join(C, 3);
     std::cout << C << std::endl;
+    std::cout << vector_index(magic_vector, std::string("b")) << std::endl;
+    std::cout << deque_index(magic_deque, 3) << std::endl;
 }
